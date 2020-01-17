7866e76f6c39100b596862fea66fcdfb70874877
Adriano Gomes <adrianogsss@gmail.com>
2020-01-17 12:06:24 -0300

    refactor: partials task renamed and docced!

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,12 +1,15 @@
 const injectPartials = require('gulp-inject-partials');
 const gulp = require('gulp');
 
-function indexPartials() {
+/**
+ * Task for building partials of every .html file into /view/
+ */
+function buildPartials() {
     return gulp.src('./view/*.html')
     .pipe(injectPartials({
         removeTags: true
      }))
     .pipe(gulp.dest('../'));
-  };
+};
   
-  exports.indexPartials = indexPartials;
+exports.buildPartials = buildPartials;
